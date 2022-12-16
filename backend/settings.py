788d48234578d4a8d51368cd2497e9d0ff8093f5
Jameskillr <amiteshjames00718@gmail.com>
2022-12-16 23:30:14 +0530

    git upt

--- backend/settings.py
+++ backend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-=^0u&!g*pbq9%h^rp9i126w7nk*!z5md7m9fa2*ezk$)x+f1#8"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
