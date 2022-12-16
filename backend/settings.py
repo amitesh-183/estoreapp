446da395e34de17a40507531d19898a6f3301b41
Jameskillr <amiteshjames00718@gmail.com>
2022-12-17 00:07:22 +0530

    vercel

--- backend/settings.py
+++ backend/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-=^0u&!g*pbq9%h^rp9i126w7nk*!z5md7m9fa2*ezk$)x+f1#8"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh''.vercel.app', '.now.sh']
 
 
 # Application definition
